3b733267e001c8a91bbfa3b4fd507527708c1d47
Jasonn <148473173+JasonFrz@users.noreply.github.com>
2025-06-15 01:34:02 +0700

    helmet.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const { connectDatabase } = require("./config/database");
-const mediaRoutes = require('./routes/media');
+const mediaRoutes = require("./routes/media");
+const helmet = require("helmet");
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use(helmet());
 
 const pokemonRoutes = require("./routes/pokemonRoutes");
 const userRoutes = require("./routes/userRoutes");
 const itemshopRoutes = require("./routes/itemshopRoutes");
 const adminRoutes = require("./routes/adminRoutes");
-const buddyRoutes = require('./routes/buddyRoutes');
-
+const buddyRoutes = require("./routes/buddyRoutes");
 
 const startServer = async () => {
   await connectDatabase();
@@ -22,8 +24,8 @@ const startServer = async () => {
   app.use("/api/v1/user", userRoutes);
   app.use("/api/v1/items", itemshopRoutes);
   app.use("/api/v1/admin", adminRoutes);
-  app.use('/api/v1/media', mediaRoutes);
-  app.use('/api/v1/buddy', buddyRoutes);
+  app.use("/api/v1/media", mediaRoutes);
+  app.use("/api/v1/buddy", buddyRoutes);
 
   app.listen(port, () =>
     console.log(`Server running at http://localhost:${port}!`)
