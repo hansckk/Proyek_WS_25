b4afa392a8a8294cee72daaef38af9c505010049
IrvinCaesar <152835521+IrvinCaesar@users.noreply.github.com>
2025-06-24 10:22:04 +0700

    tes

--- config/database.js
+++ config/database.js
@@ -5,11 +5,14 @@ dotenv.config();
 const connectDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
       dbName: 'proyek_ws_25',
     });
-    console.log("Database connected successfully!");
-  } catch (error) {
-    console.error("Database connection failed:", error.message);
+
+    console.log('✅ Connected to proyek_ws_25!');
+  } catch (err) {
+    console.error('❌ Failed to connect:', err.message);
     process.exit(1);
   }
 };
