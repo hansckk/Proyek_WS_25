c38662fe0549d47b792b13c0c96a40dc492e3eeb
IrvinCaesar <152835521+IrvinCaesar@users.noreply.github.com>
2025-06-24 10:15:24 +0700

    config/database edit

--- config/database.js
+++ config/database.js
@@ -4,13 +4,16 @@ dotenv.config();
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}`);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'proyek_ws_25',
+    });
     console.log("Database connected successfully!");
   } catch (error) {
     console.error("Database connection failed:", error.message);
     process.exit(1);
   }
 };
+
 
 const disconnectDatabase = async () => {
   try {
